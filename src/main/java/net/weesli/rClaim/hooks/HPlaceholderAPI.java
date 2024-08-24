e9ff5ccc696614ab6d5bdb495b167b4590b27514
Weesli <sefasam553498@gmail.com>
2024-08-24 13:53:43 +0300

    Added new placeholders

--- src/main/java/net/weesli/rClaim/hooks/HPlaceholderAPI.java
+++ src/main/java/net/weesli/rClaim/hooks/HPlaceholderAPI.java
@@ -2,8 +2,11 @@ package net.weesli.rClaim.hooks;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.weesli.rClaim.RClaim;
+import net.weesli.rClaim.api.RClaimAPI;
 import net.weesli.rClaim.management.ClaimManager;
 import net.weesli.rClaim.tasks.ClaimTask;
+import net.weesli.rClaim.utils.Claim;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -11,6 +14,7 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class HPlaceholderAPI extends PlaceholderExpansion {
 
@@ -42,6 +46,18 @@ public class HPlaceholderAPI extends PlaceholderExpansion {
             if (matcher.find()){
                 String claimId = matcher.group();
                 return ClaimManager.getTimeFormat(claimId);
+            }
+        }
+        if (params.equals("count")){
+            return String.valueOf((int) ClaimManager.getClaims().stream().filter(c -> c.isOwner(player.getUniqueId())).count());
+        }
+        if (params.equals("owner")){
+            if (!ClaimManager.isSuitable(player.getChunk())){
+                return "";
+            }
+            Claim claim = RClaimAPI.getInstance().getClaim(player.getChunk());
+            if (claim != null){
+                return Bukkit.getOfflinePlayer(claim.getOwner()).getName();
             }
         }
         return "";
