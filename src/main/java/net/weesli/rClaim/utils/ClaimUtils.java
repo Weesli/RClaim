f0e0ee1f3e355e69667d9a677947373eca362b89
Weesli <78960827+Weesli@users.noreply.github.com>
2025-03-01 23:25:30 +0300

    fixed error in #12

--- src/main/java/net/weesli/rClaim/utils/ClaimUtils.java
+++ src/main/java/net/weesli/rClaim/utils/ClaimUtils.java
@@ -8,16 +8,10 @@ import org.bukkit.persistence.PersistentDataType;
 public class ClaimUtils {
 
     public static Claim getClaim(Location location){
-        if (!location.getChunk().getPersistentDataContainer().has(RClaimNameSpaceKey.getKey())){
-            return ClaimManager.getClaims().stream().filter(c -> c.contains(location)).findFirst().orElse(null);
-        }
         return getClaim(location.getChunk().getPersistentDataContainer().get(RClaimNameSpaceKey.getKey(), PersistentDataType.STRING));
     }
 
     public static Claim getClaim(Chunk chunk){
-        if (!chunk.getPersistentDataContainer().has(RClaimNameSpaceKey.getKey())){
-            return ClaimManager.getClaims().stream().filter(c -> c.getChunk().equals(chunk)).findFirst().orElse(null);
-        }
         return getClaim(chunk.getPersistentDataContainer().get(RClaimNameSpaceKey.getKey(), PersistentDataType.STRING));
     }
 
