372f681945441abf6127752c3e40492a97cdb759
Weesl <sefasam553498@gmail.com>
2025-09-01 02:28:30 +0300

    clean code and syntax

--- plugin/src/main/java/net/weesli/rclaim/util/PreviewUtil.java
+++ plugin/src/main/java/net/weesli/rclaim/util/PreviewUtil.java
@@ -4,83 +4,116 @@ import net.weesli.rclaim.RClaim;
 import net.weesli.rclaim.config.ConfigLoader;
 import org.bukkit.*;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
 
 public class PreviewUtil {
 
-    public static void viewClaimRadius(Player player) {
-        previewViewer(player);
-    }
+    private static final int CHUNK_SIZE = 16;
+    private static final long DURATION_TICKS = 20L * 3;
 
-    private static void previewViewer(Player player){
-        String viewerMode = ConfigLoader.getConfig().getViewerMode();
-        int x = player.getLocation().getChunk().getX() * 16;
-        int z = player.getLocation().getChunk().getZ() * 16;
-        int size = 16;
+    private static final Map<UUID, List<PreviewBlock>> PREVIEWS = new HashMap<>();
+
+    private record PreviewBlock(Location loc, BlockData original) {}
+
+    public static void previewViewer(Player player) {
+        String mode = ConfigLoader.getConfig().getViewerMode();
         World world = player.getWorld();
-        if (viewerMode.equalsIgnoreCase("particle")){
-            startTaskWithParticles(player, player.getLocation());
-        } else if (viewerMode.equalsIgnoreCase("border")) {
-            startWorldBorderTask(player, x, z, size, world);
-        }
-    }
+        org.bukkit.Chunk chunk = player.getLocation().getChunk();
 
-    private static Particle getParticle(){
-        String currentVersion = Bukkit.getVersion();
-        if (currentVersion.equals("1.21") || currentVersion.equals("1.21.1")){
-            return Particle.valueOf("DUST");
+        int baseX = chunk.getX() * CHUNK_SIZE;
+        int baseZ = chunk.getZ() * CHUNK_SIZE;
+
+        if (mode.toLowerCase(Locale.ROOT).equals("border")) {
+            startWorldBorderTask(player, baseX, baseZ);
         } else {
-            return Particle.valueOf("REDSTONE");
+            showChunkPreview(player);
         }
     }
 
-    private static void spawnParticle(World world, Location loc) {
-        world.spawnParticle(getParticle(), loc, 10, new Particle.DustOptions(Color.RED, 1));
-    }
+    public static void showChunkPreview(Player player) {
+        clearPreview(player);
 
+        World world = player.getWorld();
+        org.bukkit.Chunk chunk = player.getLocation().getChunk();
+        int baseX = chunk.getX() * CHUNK_SIZE;
+        int baseZ = chunk.getZ() * CHUNK_SIZE;
 
-    private static void startTaskWithParticles(Player player, Location location) {
-        int x = location.getBlockX();
-        int z = location.getBlockZ();
-        int size = 16;
-        World world = location.getWorld();
-        new BukkitRunnable() {
-            int time = 20;
-            @Override
-            public void run() {
-                if (time == 0) {
-                    this.cancel();
-                    return;
+        int[][] corners = {
+                {baseX, baseZ},
+                {baseX + 15, baseZ},
+                {baseX + 15, baseZ + 15},
+                {baseX, baseZ + 15}
+        };
+
+        List<PreviewBlock> originals = new ArrayList<>();
+
+        for (int i = 0; i < corners.length; i++) {
+            int x = corners[i][0];
+            int z = corners[i][1];
+            int y = world.getHighestBlockYAt(x, z);
+
+            Location corner = new Location(world, x, y, z);
+            Block cornerBlock = corner.getBlock();
+            originals.add(new PreviewBlock(corner, cornerBlock.getBlockData()));
+            player.sendBlockChange(corner, Bukkit.createBlockData(Material.GOLD_BLOCK));
+
+            Location first, second;
+            switch (i) {
+                case 0 -> {
+                    first = corner.clone().add(1, 0, 0);
+                    second = corner.clone().add(0, 0, 1);
                 }
-                for (int i = 0; i < size * 4 - 4; i++) {
-                    int dx = i < size ? i : i < size * 2 - 1 ? size - 1 : i < size * 3 - 2 ? size * 3 - 3 - i : 0;
-                    int dz = i < size ? 0 : i < size * 2 - 1 ? i - size + 1 : i < size * 3 - 2 ? size - 1 : size * 4 - 4 - i;
-
-                    int finalX = x + dx;
-                    int finalZ = z + dz;
-                    Block block = world.getBlockAt(finalX, 0, finalZ);
-                    if (block != null) {
-                        spawnParticle(world, block.getLocation());
-                    }
+                case 1 -> {
+                    first = corner.clone().add(-1, 0, 0);
+                    second = corner.clone().add(0, 0, 1);
                 }
-                time--;
+                case 2 -> {
+                    first = corner.clone().add(-1, 0, 0);
+                    second = corner.clone().add(0, 0, -1);
+                }
+                case 3 -> {
+                    first = corner.clone().add(1, 0, 0);
+                    second = corner.clone().add(0, 0, -1);
+                }
+                default -> { continue; }
             }
-        }.runTaskTimer(RClaim.getInstance(), 0, 5);
+
+            originals.add(new PreviewBlock(first, first.getBlock().getBlockData()));
+            originals.add(new PreviewBlock(second, second.getBlock().getBlockData()));
+            player.sendBlockChange(first, Bukkit.createBlockData(Material.GLOWSTONE));
+            player.sendBlockChange(second, Bukkit.createBlockData(Material.GLOWSTONE));
+        }
+
+        PREVIEWS.put(player.getUniqueId(), originals);
+        Bukkit.getScheduler().runTaskLater(
+                RClaim.getInstance(),
+                () -> clearPreview(player),
+                DURATION_TICKS
+        );
     }
 
-    private static void startWorldBorderTask(Player player, int x, int z, int size, World world){
+    public static void clearPreview(Player player) {
+        List<PreviewBlock> list = PREVIEWS.remove(player.getUniqueId());
+        if (list == null) return;
+
+        for (PreviewBlock pb : list) {
+            player.sendBlockChange(pb.loc(), pb.original());
+        }
+    }
+
+    private static void startWorldBorderTask(Player player, int x, int z){
         WorldBorder border = Bukkit.createWorldBorder();
         border.setCenter(x+8,z+8);
         border.setSize(16);
         player.setWorldBorder(border);
         border.setSize(16,5);
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                border.reset();
-                this.cancel();
-            }
-        }.runTaskLater(RClaim.getInstance(),60);
+        Bukkit.getScheduler().runTaskLater(
+                RClaim.getInstance(),
+                () -> border.setSize(16, 3),
+                DURATION_TICKS
+        );
     }
 }
