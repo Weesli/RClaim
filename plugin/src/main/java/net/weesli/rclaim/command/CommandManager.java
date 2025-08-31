2c23daf4fcfb776dd44e1d50e9aeb5e80cf338c4
Weesl <sefasam553498@gmail.com>
2025-09-01 02:26:53 +0300

    added claims suggestion

--- plugin/src/main/java/net/weesli/rclaim/command/CommandManager.java
+++ plugin/src/main/java/net/weesli/rclaim/command/CommandManager.java
@@ -4,7 +4,9 @@ package net.weesli.rclaim.command;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
 import net.weesli.rclaim.RClaim;
+import net.weesli.rclaim.api.model.Claim;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -21,6 +23,16 @@ public class CommandManager {
     private void registerArgs() {
         commandManager.registerSuggestion(SuggestionKey.of("name"), (key,value) ->  List.of("<name>"));
         commandManager.registerSuggestion(SuggestionKey.of("amount"), (key,value) -> List.of("<amount>"));
+        commandManager.registerSuggestion(SuggestionKey.of("player_claims"), (key,value) -> {
+            Player player = (Player) key;
+            List<Claim> claims = RClaim.getInstance().getCacheManager().getClaims().getAllClaims(player.getUniqueId());
+            return claims.stream().map(claim -> {
+                if (claim.getDisplayName() == null){
+                    return claim.getID();
+                }
+                return claim.getDisplayName();
+            }).toList();
+        });
     }
 
 
